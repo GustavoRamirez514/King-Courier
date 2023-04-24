6802de672f563557e6c1030982587bcc5a57cfd3
Michelle Fernandez <fernandezm525@outlook.es>
2023-04-24 10:18:41 -0500

    se modifica el modelo de usuario que viene por defecto con djanco para añadirle tres campos, num_phone, address y city, esta modificacion del modelo se hace por medio de una nueva aplicacion de django (user). Tambien se configura en settings.py las direcciones de las carpetas templates y statics para que esten en la raiz del proyecto. En app se crean en views.py las vistas para ver informacion del usuario logeado, para que el usuario ingrese, para registrar un usuario, y para cerrar sesion, y se configura sus respectivos enlaces en el archivo urls.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render, redirect
-# Creacion y autentificacion de usuarios
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
 # crea una cookie de autenticacion
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import UserForm
 from django.contrib.auth.decorators import login_required
+# Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html')
+def perfil(request):
+    return render(request, 'perfil.html')
+
+
+def login_home(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        print(request.POST)
+        print(user)
+        if user is None:
+            return render(request, 'login.html', {
+                'error': 'usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('welcome')
+
+
+def user_create(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('perfil')
+    else:
+        form = UserForm()
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def log_out(request):
+    logout(request)
+    return redirect('login')
