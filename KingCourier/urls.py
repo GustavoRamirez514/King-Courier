25a4f422a43b6556d93387f97d922f9b7dcc5eff
Michelle Fernandez <fernandezm525@outlook.es>
2023-05-01 13:20:39 -0500

    se añade el script de la navbar en los templates y se modifica lo que se muestra en el detalle de un cliente en la vista del administrador

--- KingCourier/urls.py
+++ KingCourier/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('bienvenida/', TemplateView.as_view(template_name='welcome.html'), name='welcome'),
-    path('clientes/', include("GestionClientes.urls")),
+    path('', include("GestionClientes.urls")),
     path('mensajeros/', include("GestionMensajeros.urls")),
 ]
