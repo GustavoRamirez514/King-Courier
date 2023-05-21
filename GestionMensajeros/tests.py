25f23d6aeb2fda520ed1dddb943542b70de7a4e1
GustavoRamirez514 <tavo200079.gr@gmail.com>
2023-05-21 11:51:05 -0500

    Se agregaron pruebas para la gestionmensajeros

--- GestionMensajeros/tests.py
+++ GestionMensajeros/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase, Client
 from .models import Mensajeros
 from django.urls import reverse
+from user.models import User
 
 class MensajeroTestCase(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.client = Client()
-        self.mensajero1 = Mensajeros.objects.create(
+        self.mensajero = Mensajeros.objects.create(
             identificacion="123456789",
             nombre="Mensajero 1",
             direccion="Dirección 1",
@@ -21,7 +23,7 @@ class MensajeroTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'mensajeros/index.html')
-        self.assertContains(response, self.mensajero1.nombre)
+        self.assertContains(response, self.mensajero.nombre)
 
     def test_mensajero_sin_registros(self):
         Mensajeros.objects.all().delete()
@@ -30,3 +32,68 @@ class MensajeroTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'mensajeros/index.html')
         self.assertContains(response, "No hay mensajeros registrados")
+
+    def test_create_mensajero_get(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('create_mensajeros'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mensajeros/create.html')
+        self.assertContains(response, '<form')
+
+    def test_create_mensajero_post_valid(self):
+        self.client.force_login(self.user)
+        data = {
+            'identificacion': '9876543210',
+            'nombre': 'Jane Doe',
+            'direccion': '456 Elm St',
+            'ciudad': 'City',
+            'email': 'jane@example.com',
+            'telefono': '9876543210',
+            'vehiculo': 'Bike',
+        }
+        response = self.client.post(reverse('create_mensajeros'), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('mensajeros'))
+
+    def test_create_mensajero_post_invalid(self):
+        self.client.force_login(self.user)
+        data = {}
+        response = self.client.post(reverse('create_mensajeros'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mensajeros/create.html')
+        self.assertContains(response, 'Datos inválidos')
+
+    def test_detalle_mensajero(self):
+        response = self.client.get(reverse('detalle_mensajero', args=[self.mensajero.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mensajeros/detail.html')
+        self.assertEqual(response.context['mensajero'], self.mensajero)
+
+    def test_editar_mensajero_get(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('editar_mensajero', args=[self.mensajero.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mensajeros/edit.html')
+        self.assertContains(response, '<form')
+
+    def test_editar_mensajero_post_valid(self):
+        self.client.force_login(self.user)
+        data = {
+            'identificacion': '9876543210',
+            'nombre': 'Jane Doe',
+            'direccion': '456 Elm St',
+            'ciudad': 'City',
+            'email': 'jane@example.com',
+            'telefono': '9876543210',
+            'vehiculo': 'Bike',
+        }
+        response = self.client.post(reverse('editar_mensajero', args=[self.mensajero.id]), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('detalle_mensajero', args=[self.mensajero.id]))
+
+    def test_eliminar_mensajero(self):
+        response = self.client.get(reverse('eliminar_mensajero', args=[self.mensajero.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('mensajeros'))
+        self.mensajero.refresh_from_db()
+        self.assertFalse(self.mensajero.activo)
